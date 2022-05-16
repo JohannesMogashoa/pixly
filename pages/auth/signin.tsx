7417225b790220adc5bff63155681339412ac7b3
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-05-16 19:54:34 +0200

    Setup React with TypeScript & TailwindCSS

--- pages/auth/signin.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const SignIn: NextPage = () => {
-    return <div></div>;
-};
-
-export default SignIn;
--- pages/auth/signout.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const SignOut: NextPage = () => {
-    return <div></div>;
-};
-
-export default SignOut;
